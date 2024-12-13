85f04306d436fcf9ae111bfcfb9195c239689ea8
Laura Paez <lpaez@uoregon.edu>
2024-12-13 14:55:24 -0500

    Initial Commit

---
+++ mypetdiary/mypetdiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  mypetdiaryApp.swift
+//  mypetdiary
+//
+//  Created by Laura Páez on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct mypetdiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mypetdiaryTests/mypetdiaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  mypetdiaryTests.swift
+//  mypetdiaryTests
+//
+//  Created by Laura Páez on 12/13/24.
+//
+
+import Testing
+@testable import mypetdiary
+
+struct mypetdiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
