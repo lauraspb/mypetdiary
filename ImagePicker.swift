220a681864c552244d1a7a4e8bdacb90d93b3c27
Laura Paez <lauraspaez3@gmail.com>
2025-08-26 12:26:58 -0400

    Updated from 2024 December to 2025 August

---
+++ ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  mypetdiary
+//
+//  Created by Laura Páez on 12/13/24.
+//
+
+import Foundation
---
+++ Welcome.swift
@@ -0,0 +1,18 @@
+//
+//  Welcome.swift
+//  mypetdiary
+//
+//  Created by Laura Páez on 12/17/24.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Welcome()
+}
